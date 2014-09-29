3a479e5194c6371bf29e9e351b8fd18e653ddcfb
A-PLOUIS <antonio.pierre.louis@gmail.com>
2014-09-29 15:50:55 -0400

    Ajout de la fonction de parsing du JSON.
    Modification de AsyncTask pour qu'il traite le fichier JSON et afficher les données.
    Modification de l'adapter qui va maintenant changer les données qu'en on appuit sur Refresh

---
+++ MyApplication/app/src/main/java/pierre_louisantonio/sunshine/app/WeatherDataParser.java
@@ -0,0 +1,21 @@
+package pierre_louisantonio.sunshine.app;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by PIERRE-LOUIS Antonio on 29/09/2014.
+ */
+public class WeatherDataParser {
+
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+            throws JSONException {
+        JSONObject weather = new JSONObject(weatherJsonStr);
+        JSONArray days = weather.getJSONArray("list");
+        JSONObject dayInfo = days.getJSONObject(dayIndex);
+        JSONObject temperatureInfo = dayInfo.getJSONObject("temp");
+        return temperatureInfo.getDouble("max");
+    }
+
+}
