428ded432915081b22f5c4426d3534de8ca4f4e4
A-PLOUIS <antonio.pierre.louis@gmail.com>
2014-09-29 11:35:39 -0400

    Création d'un fichier séparé pour le fragment forecast. Renommage des clase en ForecastFragment et FetchWeatherTask

--- MyApplication/app/src/main/java/pierre_louisantonio/sunshine/app/MainActivity.java
+++ MyApplication/app/src/main/java/pierre_louisantonio/sunshine/app/MainActivity.java
@@ -1,27 +1,9 @@
 package pierre_louisantonio.sunshine.app;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -32,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -57,82 +39,6 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        public void populateListView(View rootView){
-            // Create some dummy data for the ListView.  Here's a sample weekly forecast
-            String[] data = {
-                    "Mon 6/23 - Sunny - 31/17",
-                    "Tue 6/24 - Foggy - 21/8",
-                    "Wed 6/25 - Cloudy - 22/17",
-                    "Thurs 6/26 - Rainy - 18/11",
-                    "Fri 6/27 - Foggy - 21/10",
-                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                    "Sun 6/29 - Sunny - 20/7"
-            };
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-
-            // Now that we have some dummy forecast data, create an ArrayAdapter.
-            // The ArrayAdapter will take data from a source (like our dummy forecast) and
-            // use it to populate the ListView it's attached to.
-            ArrayAdapter<String> forecastAdapter =
-                    new ArrayAdapter<String>(
-                            getActivity(), // The current context (this activity)
-                            R.layout.list_item_forecast, // The name of the layout ID.
-                            R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                            weekForecast);
-
-            // Get a reference to the ListView, and attach this adapter to it.
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(forecastAdapter);
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            populateListView(rootView);
-
-
-            new DownloaderWeatherTask().execute("http://api.openweathermap.org/data/2.5/forecast/daily?q=3570584&mode=json&units=metric&cnt=7");
-            return rootView;
-        }
-
-        //Classe se chargeant de télécharger les informations du OpenWeather
-        private class DownloaderWeatherTask extends AsyncTask<String,Void,String>{
-
-            @Override
-            protected String doInBackground(String... urls) {
-                HttpURLConnection conn = null;
-
-                try{
-                    URL url = new URL(urls[0]);
-                    conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("GET");
-                    conn.setReadTimeout(10000); // millis
-                    conn.setConnectTimeout(15000); // millis
-                    conn.setDoInput(true);
-                    conn.connect();
-                    int response = conn.getResponseCode();
-                    Log.d("HEADERHTTP", "The response is: " + response);
-
-                }catch (IOException e){
-                    Log.e("Connection","Erreur",e);
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                Log.i("ASYNC","Réussi");
-            }
-        }
-    }
 
 }
